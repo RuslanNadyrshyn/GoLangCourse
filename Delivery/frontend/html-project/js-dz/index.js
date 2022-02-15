12bb3fd7394ecd9efe9c11e047b11c6ab0b99aaa
RuslanNadyrshyn <ruslannadyrshin1999@gmail.com>
2022-02-15 21:20:33 +0200

    Edit homework for js lection

--- Delivery/frontend/html-project/js-dz/index.js
+++ Delivery/frontend/html-project/js-dz/index.js
@@ -32,59 +32,12 @@ function displayProducts(array, start) {
     }
 }
 
-const loadElements = () => new Promise((res) =>{
-    fetch("http://localhost:8080/get_products", {
-        method: "GET"
-    }).then(data => data.json().then(json_data => res(json_data)))
-})
-
-const addProduct = async () => {
-    let newMenuId = document.getElementById('product_menu_id').value;
-    let newName= document.getElementById('product_name').value;
-    let newPrice = document.getElementById('product_price').value;
-    let newImage = document.getElementById('product_image').value;
-    let newType = document.getElementById('product_type').value;
-    let newIngredients = document.getElementById('product_ingredients').value;
-    newIngredients = JSON.stringify(newIngredients);
-    let ingredients = newIngredients.split(' ');
-    console.log(newMenuId, newName, newPrice, newImage, newType, newIngredients);
-    try {
-        const response = await fetch("http://localhost:8080/add_product", {
-            method: "POST",
-            body: JSON.stringify({
-                MenuId: newMenuId,
-                Name: newName,
-                Price: newPrice,
-                Image: newImage,
-                Type: newType,
-                Ingredients: ingredients
-            }),
-            headers: {
-                'Content-type': 'application/json; charset=UTF-8',
-            },
-        })
-        const json = await response.json();
-        console.log('Успех:', JSON.stringify(json));
-        return json;
-    } catch (error) {
-        console.error('Ошибка:', error);
-    }
-}
-
 const main = async () => {
-    let array = await loadElements();
+    let array = await fetch("http://localhost:8080/get_products", { method: "GET"
+    }).then((response) => response.json())
+
     clearProducts();                 // Очищаем вывод
     displayProducts(array, 0); // Отображаем всё заново
-
-    localStorage.setItem("array", JSON.stringify(array));
 }
 
-const loop = () => {
-    new Promise((resolve) => {
-        main();
-        setTimeout(() => {
-            resolve()
-        }, 1000)
-     }).then(loop)
-}
- loop()
+main();
