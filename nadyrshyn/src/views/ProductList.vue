8cec18c4d54fe4ac1f7c953f10ce130fcfc20cf4
RuslanNadyrshyn <ruslannadyrshin1999@gmail.com>
2022-02-18 02:46:48 +0200

    Add product navigation items

--- nadyrshyn/src/views/ProductList.vue
+++ nadyrshyn/src/views/ProductList.vue
@@ -6,19 +6,30 @@
           {{ error }}
         </div>
       </template>
-
-      <div class="Product_list">
-        <div v-for="product in products" :key="product.id">
-          <Product
-            :id="product.id"
-            :name="product.name"
-            :image="product.image"
-            :price="product.price"
-            :type="product.type"
-            :ingredients="product.ingredients"
-          >
-            {{ products }}
-          </Product>
+      <Header></Header>
+      <div class="intro">
+        <div class="section">
+          <div class="container">
+            <div class="product_list_block">
+              <div v-for="product in products" :key="product.id">
+                <ProductListNav :type="product.type"></ProductListNav>
+              </div>
+              <div class="Product_list">
+                <div v-for="product in products" :key="product.id">
+                  <Product
+                    :id="product.id"
+                    :name="product.name"
+                    :image="product.image"
+                    :price="product.price"
+                    :type="product.type"
+                    :ingredients="product.ingredients"
+                  >
+                    {{ products }}
+                  </Product>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
@@ -29,18 +40,34 @@
 <script>
 import Product from "@/components/Product";
 import axios from "axios";
+import Header from "@/components/Header";
+import ProductListNav from "@/components/ProductListNav";
 
 export default {
   name: "ProductList",
-  components: { Product },
+  components: { ProductListNav, Header, Product },
   data() {
     return {
+      suppliers: [],
       products: [],
       loaded: false,
       errors: [],
     };
   },
   mounted() {
+    // const suppliers_url = "http://localhost:8082/get_supppliers";
+    // axios
+    //     .get(suppliers_url)
+    //     .then((res) => {
+    //       this.suppliers = res.data;
+    //       console.log(this.suppliers);
+    //     })
+    //     .catch((err) => {
+    //       this.errors.push(err);
+    //     })
+    //     .finally(() => {
+    //       this.loaded = true;
+    //     });
     const url = "http://localhost:8082/get_products";
     axios
       .get(url)
@@ -59,6 +86,34 @@ export default {
 </script>
 
 <style scoped>
+.intro {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  width: 100%;
+  background-color: #ffcfb4;
+  background-size: cover;
+}
+.product_list_block {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  background-color: #686e65;
+  border-radius: 20px;
+  margin-top: 10px;
+}
+.product_list_nav {
+  display: flex;
+  font-size: 20px;
+  background-color: coral;
+  min-height: 50px;
+  border-radius: 14px 10px 0 0;
+}
+.container {
+  width: 100%;
+  max-width: 1200px;
+  margin: 0 auto;
+}
 .Product_list {
   display: flex;
   flex-wrap: wrap;
