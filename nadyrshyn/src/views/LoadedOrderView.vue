397cd4214bd7c6f687c11dbef98e14b583bfa9b2
RuslanNadyrshyn <ruslannadyrshin1999@gmail.com>
2022-05-03 03:58:30 +0300

    Edit order item

--- nadyrshyn/src/views/LoadedOrderView.vue
+++ nadyrshyn/src/views/LoadedOrderView.vue
@@ -1,38 +1,49 @@
 <template>
   <div class="intro">
     <template v-if="$store.state.orders.loaded">
-      <template v-if="$store.state.orders.errors.length">
-        <div v-for="(error, index) in $store.state.orders.errors" :key="index">
-          {{ error }}
+      <template
+        v-if="
+          $store.getters['auth/getUserID'] == $store.state.orders.order.userId
+        "
+      >
+        <template v-if="$store.state.orders.errors.length">
+          <div
+            v-for="(error, index) in $store.state.orders.errors"
+            :key="index"
+          >
+            {{ error }}
+          </div>
+        </template>
+        <div class="section">
+          <div class="container">
+            <div class="title">
+              <label class="title_text">
+                Заказ №{{ $store.state.orders.order.id }}
+              </label>
+            </div>
+            <div class="basket_block">
+              <div
+                v-for="product in this.$store.getters[
+                  'orders/getOrderProducts'
+                ]"
+                :key="product.id"
+              >
+                <OrderItem :product="product"></OrderItem>
+              </div>
+            </div>
+            <div class="total_price_block">
+              <div class="total_price_container">
+                Всего:
+                <div class="total_price">
+                  {{ $store.state.orders.order.price }}
+                </div>
+                грн
+              </div>
+              <label>{{ $store.state.orders.order.created_at }}</label>
+            </div>
+          </div>
         </div>
       </template>
-      <div class="section">
-        <div class="container">
-          <div class="title">
-            <label class="title_text">
-              Заказ №{{ $store.state.orders.order.id }}
-            </label>
-          </div>
-          <div class="basket_block">
-            <div
-              v-for="product in this.$store.getters['orders/getOrderProducts']"
-              :key="product.id"
-            >
-              <OrderItem :product="product"></OrderItem>
-            </div>
-          </div>
-          <div class="total_price_block">
-            <div class="total_price_container">
-              Всего:
-              <div class="total_price">
-                {{ $store.state.orders.order.price }}
-              </div>
-              грн
-            </div>
-            <label>{{ $store.state.orders.order.created_at }}</label>
-          </div>
-        </div>
-      </div>
     </template>
     <label v-else class="loading">Загрузка...</label>
   </div>
@@ -46,7 +57,6 @@ export default {
   components: { OrderItem },
   created() {
     this.$store.dispatch("orders/fetchOrder", this.$route.params.id);
-    console.log(this.$store.getters["orders/getOrder"]);
   },
 };
 </script>
